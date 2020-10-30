bcec691cd5ff5aa2407371c128aab723c43275bb
cozyDoomer <chr.unterrainer@gmail.com>
2020-10-30 16:15:03 +0100

    fix formatting errors, url in test_search_character add tests for all objects

--- controller/exceptions.py
+++ controller/exceptions.py
@@ -4,27 +4,30 @@ class CharacterNotFoundException(Exception):
         self.message = f'Character {name} not found.'
         super(CharacterNotFoundException, self).__init__(self.message)
 
+
 class MangaNotFoundException(Exception):
 
     def __init__(self, name):
         self.message = f'Manga {name} not found.'
         super(MangaNotFoundException, self).__init__(self.message)
 
+
 class AnimeNotFoundException(Exception):
 
     def __init__(self, name):
         self.message = f'Anime {name} not found.'
         super(AnimeNotFoundException, self).__init__(self.message)
+
+
+class PersonNotFoundException(Exception):
+
+    def __init__(self, name):
+        self.message = f'Person {name} not found.'
+        super(PersonNotFoundException, self).__init__(self.message)
+
 
 class ServiceUnavailable(Exception):
 
     def __init__(self):
         self.message = 'Service Unavailable.'
         super(ServiceUnavailable, self).__init__(self.message)
-
-class AnimeNotFoundException(Exception):
-
-    def __init__(self, name):
-        self.message = f'Anime {name} not found.'
-        super(AnimeNotFoundException, self).__init__(self.message)
-
